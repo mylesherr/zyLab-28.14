ac887bee0c983d0d56ef24d0cc1035703f0a3259
mylesherr <167835533+mylesherr@users.noreply.github.com>
2024-05-02 22:08:36 -0500

    Update ItemToPurchase.h

--- ItemToPurchase.h
+++ ItemToPurchase.h
@@ -1,1 +1,36 @@
+/* Type your code here */
 
+#ifndef ITEMTOPURCHASE_H
+#define ITEMTOPURCHASE_H
+
+#include <string>
+using namespace std;
+
+class ItemToPurchase {
+public:
+    ItemToPurchase();
+    ItemToPurchase(string name, string description, int price = 0, int quantity = 0);
+
+    void SetName(string description);
+    string GetName() const;
+
+    void SetDescription(string name);
+    string GetDescription() const;
+
+    void SetPrice(int price);
+    int GetPrice() const;
+
+    void SetQuantity(int quantity);
+    int GetQuantity() const;
+
+    void PrintItemCost() const;
+    void PrintItemDescription() const;
+
+private:
+    string itemName;
+    string itemDescription;
+    int itemPrice;
+    int itemQuantity;
+};
+
+#endif       
